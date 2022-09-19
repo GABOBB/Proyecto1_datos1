53fdd692985edb91a441260789c033c1b2f97321
Gabriel Bolaños <gabbol2002@gmail.com>
2022-09-18 21:32:54 -0600

    avance_III

---
+++ PROYECTO_I/src/modelos/cancion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class cancion {
+    private String nombre;
+    
+    
+    
+}

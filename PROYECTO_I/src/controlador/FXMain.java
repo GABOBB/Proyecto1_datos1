30fd1f2377b92f70bb5f66fb716c83fb5fb40453
Gabriel Bolaños <gabbol2002@gmail.com>
2022-09-18 21:24:40 -0600

    avance_I

---
+++ PROYECTO_I/src/controlador/FXMain.java
@@ -0,0 +1,42 @@
+package controlador;
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class FXMain extends Application {
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(FXMain.class.getResource("/vista/InSe.fxml"));
+            Pane ventana = (Pane) loader.load();
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            InSeC controlador = loader.getController();
+            controlador.setStage(primaryStage);
+            primaryStage.show();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        launch(args);
+        
+    }
+    
+}
